fd1069e676e072c0089c4ae10df5e871e35aa3fd
Daniel Bright <brahyt@gmail.com>
2019-09-20 07:46:12 -0700

    eslint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let STORE = [
 function createStringWith(item, index){
   return `
   	<li data-item-id="${item.id}">
-      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
@@ -20,15 +20,15 @@ function createStringWith(item, index){
             <span class="button-label">delete</span>
         </button>
       </div>
-    </li>`
+    </li>`;
 }
 function listItemsString(list) {
-  let mapOfStuff = list.map((e, i) => createStringWith(e, i))
-  return mapOfStuff
+  let mapOfStuff = list.map((e, i) => createStringWith(e, i));
+  return mapOfStuff;
 }
 function renderShoppingList() {
   let shoppingItemsString = listItemsString(STORE); 
-  $('.js-shopping-list').html(shoppingItemsString)
+  $('.js-shopping-list').html(shoppingItemsString);
 }
 function handleNewItemSubmit() {
   $('#js-shopping-list-form button').on('click', e => {
@@ -38,34 +38,33 @@ function handleNewItemSubmit() {
     renderShoppingList();
   });
 }
-
 function toggleCheck(idOfLi){
   return STORE.find(e => {
     if (e.id === idOfLi){
       e.checked = !e.checked;
     }
-  })
+  });
 }
 function handleItemCheckClicked() {
   $('.js-shopping-list').on('click', '.js-item-toggle', e => {
-    let idOfItem = $(e.currentTarget).closest('li').data('item-id')
+    let idOfItem = $(e.currentTarget).closest('li').data('item-id');
     toggleCheck(idOfItem); 
     renderShoppingList();
-  })
+  });
 }
 function toggleDelete(idOfLi){
   return STORE.find((e, i)  => {
     if (e.id === idOfLi){
-      STORE.splice(i, 1)
-      renderShoppingList()
+      STORE.splice(i, 1);
+      renderShoppingList();
     }
-  })
+  });
 }
 function handleDeleteItemClicked() {
   $('.js-shopping-list').on('click', '.js-item-delete', e => {
-    let idOfItem = $(e.currentTarget).closest('li').data('item-id')
+    let idOfItem = $(e.currentTarget).closest('li').data('item-id');
     toggleDelete(idOfItem); 
-  })
+  });
 }
 function handleShoppingList() {
   renderShoppingList();
